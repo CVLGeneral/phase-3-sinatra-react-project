ab24e092cf3bf380d8764f7fba2a5d9646b74cc4
Leonard Kiprotich <cvlgeneral2020@gmail.com>
2023-06-09 21:06:56 +0300

    Done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_160734) do
+ActiveRecord::Schema.define(version: 2023_06_09_180502) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2023_06_05_160734) do
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
-    t.string "type"
     t.string "genre"
     t.string "description"
     t.datetime "release_date"
